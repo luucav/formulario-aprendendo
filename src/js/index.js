c6cdaa1e73849da1fc492651d5167d588aef7b5d
luucav <80016546+luucav@users.noreply.github.com>
2022-03-13 22:59:08 -0300

    form

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+const init = () => {
+    const inputEmail = document.querySelector('input[type="email"]');
+    const inputPassword = document.querySelector('input[type="password"]')
+    const submitButton = document.querySelector('.login-submit')
+
+
+    if(submitButton){
+        submitButton.addEventListener['click'], (event) =>{
+            event.preventDefaut();
+            fetch('https://reqres.in/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: inputEmail.value,
+                    password: inputPassword.value,
+                })
+            }).then((response) => {
+                return response.json();
+            }).then((data) => {
+                console.log(data)
+            })
+        }
+    }
+}
+
+window.onload = init;
