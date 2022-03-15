e60883b25742fc8d11e4ddceeb92d9ba6ce009dd
luucav <80016546+luucav@users.noreply.github.com>
2022-03-15 09:27:52 -0300

    concertado Java script

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,27 @@
 const init = () => {
-    const inputEmail = document.querySelector('input[type="email"]');
-    const inputPassword = document.querySelector('input[type="password"]')
-    const submitButton = document.querySelector('.login__submit')
 
+    const validateEmail = (event) => {
+        const input = event.currentTarget;
+        const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+        const emailTest = regex.test(input.value);
+        
+        if(emailTest) {
+            submitButton.setAttribute('disabled', 'disabled');
+            input.nextElementSibling.classlist.add('error');
+        } else {
+            submitButton.removeAttribute('disabled');
+            input.nextElementSibling.classlist.remove('error');
+        }
+    }
+
+
+
+
+    const inputEmail = document.querySelector('input[type="email"]');
+    const inputPassword = document.querySelector('input[type="password"]');
+    const submitButton = document.querySelector('.login__submit');
+
+    inputEmail.addEventListener('input', validateEmail);
 
     if(submitButton){
         submitButton.addEventListener['click'], (event) => {
